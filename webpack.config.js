8f244c52b6093e1ca2b911ad27a9616129b9f22c
Andrew Gunkel <65040985+andrewgunkel@users.noreply.github.com>
2026-02-24 07:44:21 +0100

    config cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ import path from "node:path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
-  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
@@ -14,6 +13,7 @@ export default {
   devtool: "eval-source-map",
   devServer: {
     watchFiles: ["./src/template.html"],
+    open: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
